e91b5eca2c2f253329b77e7b4454266b07ac4490
Andy Jones <aejones89@gmail.com>
2017-10-21 14:33:32 +0100

    Change the response structure

--- microservice-users/project/views.py
+++ microservice-users/project/views.py
@@ -12,14 +12,20 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+response_object = dict(
+    status='success'
+)
+
 def list_users_view(session: Session) -> typing.List[UserType]:
-    """List all the users and serialize the response."""
+    """List all the users on in the database."""
     queryset = session.query(User).all()
-    return [UserType(record) for record in queryset]
+    response_object['data'] = [UserType(record) for record in queryset]
+
+    return Response(response_object, status=200)
 
 
 def create_user_view(session: Session, user: UserType) -> Response:
-
+    """Create a new user with a given username and password."""
     username = user.get('username')
     email = user.get('email')
 
@@ -27,23 +33,26 @@ def create_user_view(session: Session, user: UserType) -> Response:
     session.add(user)
     session.flush()
 
-    data = dict(
+    response_object['data'] = dict(
         id=user.id,
         username=user.username,
         email=user.email
     )
 
-    return Response(data, status=201)
+    return Response(response_object, status=201)
 
 
 def detail_user_view(session: Session, user_id: IdType) -> Response:
-
+    """Get details about a single user."""
     user = session.query(User).get(user_id)
-
-    # data = dict(
-    #     id=user.pk,
-    #     username=user.username,
-    #     email=user.email
-    # )
-    data = str(user_id)
-    return Response(data, status=200)
+    if user:
+        response_object['data'] = dict(
+            id=user.id,
+            username=user.username,
+            email=user.email
+        )
+        return Response(response_object, status=200)
+    else:
+        response_object['status'] = 'fail'
+        response_object['message'] = 'User does not exist'
+        return Response(response_object, status=204)
